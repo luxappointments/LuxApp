065079e14f61c9b08a3451b76ce0d1f7652233af
killeenpickup <killeenpickup@gmail.com>
2026-02-04 20:25:01 -0600

    Make Supabase client singleton

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,6 +2,10 @@
 
 import { createClient } from "@supabase/supabase-js";
 
+let browserClient: ReturnType<typeof createClient> | null = null;
+
 export function getClientSupabase() {
-  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+  if (browserClient) return browserClient;
+  browserClient = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+  return browserClient;
 }
